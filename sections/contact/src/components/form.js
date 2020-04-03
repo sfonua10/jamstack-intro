d97c542407bbcbfa98b7231f3ad7a9981aa13988
Siosaia Fonua <sfonua10@churchofjesuschrist.org>
2020-04-02 20:54:21 -0600

    form component and lambda func

---
+++ sections/contact/src/components/form.js
@@ -0,0 +1,68 @@
+import React, { useReducer } from 'react';
+import styles from './form.module.css';
+//How to update this state?
+//use reducer. Becuase it would be a big pain to independetly tracked state. If we wanted to reset, we would have to call 4 different functions for each to be empty. We're going to treat whole state as a single value and peform differnt actions on it based on what we send in.
+//Reduce, define function called reducer. Reducer gets current state and action, action is defined by us. Standard way to approach this is to use a switch statement. 
+const INITIAL_STATE = {
+  name: '',
+  email: '',
+  subject: '',
+  body: ''
+}
+//standard way to set up action is to set up type
+// {type: 'dostuff', name: 'Saia'}
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'updateFieldValue':
+      return { ...state, [action.field]: action.value};
+    default:
+      return INITIAL_STATE;
+  }
+}
+const Form = () => {
+  //dispatcher is how you send an action to a reducer
+  //state is the INITIAL STATE or whatever gets returned from reducer
+  const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
+
+  console.log({ state });
+
+  const updateFieldValue = field => event => {
+    dispatch({
+      type: 'updateFieldValue',
+      field,
+      value: event.target.value
+    })
+  }
+
+  const handleSubmit = event => {
+    // so it doesn't reload page
+    event.preventDefault();
+
+    //now we've opted in to that responsibility that we need to handle now
+    //TODO actually send the message
+    console.log(state)
+  }
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <label className={styles.label}>
+        Name
+        <input className={styles.input} type="text" name="name" value={state.name} onChange={updateFieldValue('name')} /> 
+      </label>
+      <label className={styles.label}>
+        Email
+        <input className={styles.input} type="email" name="email" value={state.email} onChange={updateFieldValue('email')}/>
+      </label>
+      <label className={styles.label}>
+        Subject
+        <input className={styles.input} type="text" name="subject" value={state.subject} onChange={updateFieldValue('subject')}/>
+      </label>
+      <label className={styles.label}>
+        Body
+        <textarea className={styles.input} name="email" value={state.body} onChange={updateFieldValue('body')} />
+      </label>
+      <button className={styles.button}>Send</button>
+    </form>
+  )
+}
+
+export default Form;
